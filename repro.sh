5e8cf4a6241f3de0ffecc98afe2c27d329081539
Tomas Aschan <tomasl@spotify.com>
2022-07-12 14:06:58 +0200

    Fix setup

--- repro.sh
+++ repro.sh
@@ -8,4 +8,7 @@ fi
 
 gsutil cat gs://config-management-release/released/1.12.0/config-management-operator.yaml | kubectl apply -f -
 
-kubectl apply -f config-management-setup
+kubectl apply -f config-management-setup/config-management.yaml
+while ! kubectl get crd rootsyncs.configsync.gke.io >/dev/null 2>&1; do sleep 1; done
+kubectl wait crd rootsyncs.configsync.gke.io --for condition=established
+kubectl apply -f config-management-setup/root-sync.yaml
